d43a8ce1d4ec24652fb926d5db5830a87c272643
Iron Tom <irontom233@gmail.com>
2024-08-11 09:31:06 -0700

    stage 3: navgation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-
 import { cn } from "@/lib/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { SiteHeader } from "@/components/site-header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,12 +25,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           fontSans.variable
         )}
       >
-        
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-        
+        <div className="relative flex min-h-dvh flex-col bg-background">
+          <SiteHeader />
+          <main className="flex-1">{children}</main>
+        </div>
       </body>
     </html>
   );
---
+++ src/components/site.tsx
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Iron Tom's Blog",
+  url: "https://example.com",
+  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  author: "Iron Tom",
+  links: {
+    twitter: "https://x.com/iron_tom233",
+    github: "https://github.com/unravelTao",
+    personalSite: "https://personal-portfolio-topaz-theta.vercel.app/",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
